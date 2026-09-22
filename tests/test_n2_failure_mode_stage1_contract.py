a0effa8813119f1e1985e88c5f78ce463416e2fb
ZHANG RUIQI <rachelzhang0223@gmail.com>
2026-09-23 00:20:37 +0900

    fix: clean stage1 contract test syntax

--- tests/test_n2_failure_mode_stage1_contract.py
+++ tests/test_n2_failure_mode_stage1_contract.py
@@ -50,7 +50,8 @@ def test_factorial_grid_has_exact_predeclared_size_and_structural_exclusions():
     assert design["eligible_factorial_cell_count"] == 864
     assert excluded_pairs == {(5, 4), (5, 10), (12, 10)}
     assert {tuple(x) for x in design["structurally_excluded_group_layer_pairs"]} == excluded_pairs
-    assert design["replicates_per_factorial_cell"] == 64\n    assert design["population_interval_bootstrap_draws_per_world"] == 500
+    assert design["replicates_per_factorial_cell"] == 64
+    assert design["population_interval_bootstrap_draws_per_world"] == 500
 
 
 def test_bop_calibration_is_predeclared_and_in_realistic_window():
@@ -78,7 +79,9 @@ def test_primary_inference_uses_group_cv_population_mean_not_unanimity():
     assert rule["confidence_level"] == 0.95
     assert rule["interval"]["switch_threshold_group_count"] == 10
     assert "Student t interval" in rule["interval"]["fewer_than_10_independent_groups"]
-    assert "percentile bootstrap" in rule["interval"]["at_least_10_independent_groups"]\n    assert rule["interval"]["factorial_bootstrap_draws"] == 500\n    assert rule["interval"]["confirmatory_anchor_bootstrap_draws"] == 4000
+    assert "percentile bootstrap" in rule["interval"]["at_least_10_independent_groups"]
+    assert rule["interval"]["factorial_bootstrap_draws"] == 500
+    assert rule["interval"]["confirmatory_anchor_bootstrap_draws"] == 4000
     assert rule["positive_transfer"] == "lower confidence bound > 0"
     assert rule["unanimity_rule_used"] is False
 
